f269795b16c5bf6f5852bd87c9a694e73e01f1fc
JungWook Nam <jwnamid@gmail.com>
2023-09-27 17:16:06 +0900

    Set screen size

--- G-avoid.py
+++ G-avoid.py
@@ -1,4 +1,10 @@
-import pygame
+import pygame git check # 파이게임 라이브러리 불러옴
 import random
 
-pygame.init()
+pygame.init() #초기화
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_height = 640 # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
