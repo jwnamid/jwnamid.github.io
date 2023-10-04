6d819e8abc7ac7132deba9e73eab931987b14ad3
chaelimee <bincl418@naver.com>
2023-10-04 17:19:21 +0900

    게임창 생성

---
+++ G-snake.py
@@ -0,0 +1,15 @@
+# Make G-snake.py
+import pygame
+
+pygame.init()
+dis=pygame.display.set_mode((800,600))
+pygame.display.update()
+pygame.display.set_caption('Snake game')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over=True
+ 
+pygame.quit()
+quit()
