6492142ce08e2d53a0a6c10b16499dc7dd8bdb37
JungWook Nam <jwnamid@gmail.com>
2023-10-04 16:46:15 +0900

    Add game sources

--- G-avoid.py
+++ G-avoid.py
@@ -8,3 +8,14 @@ screen_width = 480 # 가로 크기
 screen_height = 640 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+# 화면 타이틀 설정
+pygame.display.set_caption("낙하물 피하기")
+
+# FPS
+clock = pygame.time.Clock()
+
+# 사용자 게임 초기화 (배경화면, 게임 이미지, 좌표, 속도, 폰트 등)
+background = pygame.image.load("C:/git-workspace/pygame-SJ/back.png")
+character = pygame.image.load("C:/git-workspace/pygame-SJ/player_small.png:)
+enemy = pygame.image.load("C:/git-workspace/pygame-SJ/enemy_small.png")
+
