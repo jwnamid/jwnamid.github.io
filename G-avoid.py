fc263e3786d8177838dfe00c1d6f2b02ec5715f3
JungWook Nam <jwnamid@gmail.com>
2023-11-20 17:25:59 +0900

    화면 오류 수정

--- G-avoid.py
+++ G-avoid.py
@@ -4,8 +4,8 @@ import random
 pygame.init() #초기화
 
 # 화면 크기 설정
-screen_width = 480 # 가로 크기
-screen_height = 640 # 세로 크기
+screen_width = 500 # 가로 크기
+screen_height = 500 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 화면 타이틀 설정
