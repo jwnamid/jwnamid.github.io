c107819eeec9d75b874a756f63b5691adcf0ec83
parkjeonghyeon1013 <pkys1020@naver.com>
2023-11-27 17:16:06 +0900

    snake 게임  연결됨 / 버튼을 통해 열리도록 수정 필요

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from G_baseballGame import baseballGame
 #from G_avoid import avoidGame
-#from G_snake import snakeGame
+from G_snake import snakeGame
 from G_memoryGame import memoryGame
 
 
-g = baseballGame()
+g = snakeGame()
 
 while g.running:
-    g.curr_menu.display
     g.game_loop()
+
+    
